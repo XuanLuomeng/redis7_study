d395209bb390e0798ebcdd9cc325fdb4e4328435
XuanLuomeng <1079716693@qq.com>
2023-08-15 23:44:00 +0800

    1.0.2

---
+++ src/main/java/com/gduf/redis7_study/service/HyperLogLogService.java
@@ -0,0 +1,13 @@
+package com.gduf.redis7_study.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author LuoXuanwei
+ * @date 2023/8/15 23:43
+ */
+@Service
+@Slf4j
+public class HyperLogLogService {
+}
